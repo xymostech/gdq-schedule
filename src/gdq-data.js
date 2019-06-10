88891c6299f5ddb270adb3a637fa8a1e5dbe2aa8
Emily Eisenberg <xymostech@gmail.com>
2019-06-10 15:32:48 -0700

    Update for SGDQ 2019

--- src/gdq-data.js
+++ src/gdq-data.js
@@ -1,18 +1,24 @@
 export const current = {
-    name: "Awesome Games Done Quick 2019",
-    color: "blue",
+    name: "Summer Games Done Quick 2019",
+    color: "red",
     scheduleDataFile: "schedule.json",
-    startTime: "2019-01-06T16:30:00Z",
+    startTime: "2019-06-23T16:30:00Z",
     showCurrentTime: true,
 };
 
 // This should be in reverse chronological order. Put new events at the top,
 // not the bottom.
 export const past = {
+    "agdq-2019": {
+        name: "Awesome Games Done Quick 2019",
+        color: "blue",
+        scheduleDataFile: "agdq-2019.json",
+        startTime: "2019-01-06T16:30:00Z",
+    },
     "sgdq-2018": {
         name: "Summer Games Done Quick 2018",
         color: "red",
-        scheduleDataFile: "schedule.json",
+        scheduleDataFile: "sgdq-2018.json",
         startTime: "2018-06-24T16:30:00Z",
     },
     "agdq-2018": {
